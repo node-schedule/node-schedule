31e7fab4343a0e449cb7a7b558e5de580aff0abd
Anthony McCormick <=>
2013-11-02 21:19:43 +0000

    Removed Date.prototype methods as it is not a good idea to extend global objects, Added the ability to pass in date strings as valid schedule times

---
+++ test/date-convenience-methods-test.js
@@ -0,0 +1,40 @@
+var main = require('../package.json').main;
+var schedule = require('../' + main);
+
+module.exports = {
+    "Date Enhancements":{
+        "Should Not add date convenience methods unless explicitly specified": function(test){
+            test.ok(typeof Date.addYear !== 'function');
+            test.ok(typeof Date.addMonth !== 'function');
+            test.ok(typeof Date.addDay !== 'function');
+            test.ok(typeof Date.addHour !== 'function');
+            test.ok(typeof Date.addMinute !== 'function');
+            test.ok(typeof Date.addSecond !== 'function');
+            test.done();
+        },
+        "Should add date convenience methods when explicitly specified": function(test){
+            schedule.addDateConvenienceMethods(Date);
+            test.ok(typeof Date.addYear === 'function');
+            test.ok(typeof Date.addMonth === 'function');
+            test.ok(typeof Date.addDay === 'function');
+            test.ok(typeof Date.addHour === 'function');
+            test.ok(typeof Date.addMinute === 'function');
+            test.ok(typeof Date.addSecond === 'function');
+            test.done();
+        }
+    },
+    "Date string":{
+        "Should accept a valid date string": function(test){
+            test.expect(1);
+
+            schedule.scheduleJob(new Date(Date.now() + 1000).toString(), function() {
+                test.ok(true);
+            });
+
+            setTimeout(function() {
+                test.done();
+            }, 1250);
+        }
+    }
+};
+
