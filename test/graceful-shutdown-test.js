0dbc1341b0332862cbc1e7eac1468b7639a537a4
changmyeong <changmyeong.baek@gmail.com>
2021-11-29 02:23:57 +0900

    delete no use test case

--- test/graceful-shutdown-test.js
+++ test/graceful-shutdown-test.js
@@ -4,12 +4,6 @@ const test = require('tape');
 const schedule = require('..');
 
 test('Graceful Shutdown', function (t) {
-  t.test('close immediately when no job', function (test) {
-    schedule.gracefulShutdown().then(function() {
-      test.end();
-    });
-  });
-
   t.test('pending when job running', function (test) {
     test.plan(1);
     const job = schedule.scheduleJob('* * * * * *', function () {
